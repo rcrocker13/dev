4c54cd8c3831c99f5b3e6bbc068237652cce5e4c
rcrocker13 <rcrocker13@gmail.com>
2013-10-25 19:50:47 -0400

    Update runningTotal.py

--- runningTotal.py
+++ runningTotal.py
@@ -1,9 +1,20 @@
+# While Loop Construct
+
 i = 0
 agg = 0
 end = 6
 
 while i < end:
     i += 1
+    agg += i
+    
+print(agg)
+
+# For Loop Construct
+agg = 0
+end = 6
+
+for i in range(end + 1):
     agg += i
     
 print(agg)
